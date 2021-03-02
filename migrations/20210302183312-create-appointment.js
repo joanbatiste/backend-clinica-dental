c9b11319452a432f281f7a190de21861f41ee452
Joan <joanpulle@gmail.com>
2021-03-02 19:34:29 +0100

    Created all models

---
+++ migrations/20210302183312-create-appointment.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Appointments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      customerId: {
+        type: Sequelize.INTEGER
+      },
+      dentistId: {
+        type: Sequelize.INTEGER
+      },
+      appointmentDate: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Appointments');
+  }
+};
