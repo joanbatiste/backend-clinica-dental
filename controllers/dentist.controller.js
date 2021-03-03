c5131e3bde14373c3abbcd2ad08f0c97d31005f3
Joan <joanpulle@gmail.com>
2021-03-03 23:34:44 +0100

    All CRUD dentist functional but not fine

--- controllers/dentist.controller.js
+++ controllers/dentist.controller.js
@@ -6,14 +6,28 @@ class DentistController {
     constructor() {
 
     }
-    
+    //Traer todos los dentistas
     async indexAll(){
         return Dentist.findAll();
     }
+    //Traer un dentista
     async indexOne(id){
-        return Dentist.findById({where:id});
+        return Dentist.findOne({where:{id}});
     }
-
+    //Dar de alta un dentista
+    async createOne(dentist){
+        return Dentist.create(dentist);
+    }
+    //Actualizar datos de un dentista
+    async updateDentist(id, dentist) {    
+        console.log(id, dentist);
+        return Dentist.update(dentist,{where:{id}},{new: true});
+        
+    };
+    //Eliminar un dentista
+    async destroyDent(id){
+        return Dentist.destroy({where:{id:id}});
+    }
     
 
 
--- routes/dentist.router.js
+++ routes/dentist.router.js
@@ -8,12 +8,14 @@ const router = require("express").Router();
 const dentistController = require("../controllers/dentist.controller");
 
 
+
+
 //Recursos anidados de dentistas
 
 // router.use("/:id/appointments", appointmentRouter);
 
 // EndPoints de dentistas
-
+//Recuperar todos los dentistas
 router.get("/", async (req, res)=>{
     try{
 
@@ -26,10 +28,11 @@ router.get("/", async (req, res)=>{
 
     };
 });
+
+//Buscar un dentista por su id
 router.get("/:id", async (req,res)=>{
     try{
-        const id = req.params.id;
-        res.json(await dentistController.indexOne(id));
+        res.json(await dentistController.indexOne(req.params.id));
 
     }catch(error){
         res.status(500).json({
@@ -38,7 +41,45 @@ router.get("/:id", async (req,res)=>{
 
     };
 });
+//Dar de alta un dentista
+router.post("/", async(req, res)=>{
+    try{
+        const createdDentist = await dentistController.createOne(req.body);
+        res.status(200).json(createdDentist);
 
+    }catch(error){
+        return res.status(500).json({
+            message: "Server error"
+        });
+    };
+});
+
+//Actualizar datos de un dentista
+router.put("/:id", async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const updatedDentist = await dentistController.updateDentist(id, req.body);
+        res.status(200).json(updatedDentist);
+        console.log("soy el dentista actualizado",updatedDentist);
+
+    }catch(error){
+        return res.status(500).json({
+            message: "Server error"
+        });
+
+    };
+});
+
+//Eliminar un registro de dentista
+router.delete("/:id", async(req,res)=>{
+    try{
+        res.json(await dentistController.destroyDent(req.params.id));
+    }catch(error){
+        return res.status(500).json({
+            message: "Server error"
+        });
+    };
+});
 
 module.exports = router;
 
