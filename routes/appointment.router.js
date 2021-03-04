b7d314a0a94760b0140fb55fa5725439e7e17678
Joan <joanpulle@gmail.com>
2021-03-04 22:36:48 +0100

    login added to customers

--- routes/appointment.router.js
+++ routes/appointment.router.js
@@ -35,3 +35,5 @@ router.post("/", async (req, res)=>{
         });
     };
 });
+
+module.exports = router;
--- routes/customer.router.js
+++ routes/customer.router.js
@@ -35,8 +35,9 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
     try{
-        
-        res.json(await customerController.createCustomer(req.body));
+        const customer = await customerController.createCustomer(req.body);
+        const status = "Success";
+        res.json({status,customer});
 
     }catch(error){
         res.status(500).json({
@@ -44,6 +45,18 @@ router.post("/", async (req, res) => {
         });
     };
 
+});
+
+router.post("/login", async (req,res)=>{
+    try{
+        const {email,password} = req.body;
+        const jwt = await customerController.login(email,password);
+        res.json({jwt})
+    }catch(error){
+        return res.status(401).json({
+            message: error.message
+        });
+    };
 });
 
 module.exports = router;
